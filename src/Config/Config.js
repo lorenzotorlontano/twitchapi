72f9b947b83c719a36b0db478be5c280eca0f386
tuonome <lorenzo.torlontano@gmail.com>
2020-09-21 15:29:31 +0200

    flow finished piero collaboratore di merda

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,6 +1,6 @@
-export const authEndpoint = "https://id.twitch.tv/oauth2/authorize";
+export const authEndpoint = "https://id.twitch.tv/oauth2/token";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "8uv5uigx4woona52bd4104ev464565";
-export const redirectUri = "http://localhost:3000/Hash";
-export const responseType = "code";
+export const secret = "wylnlq7zqrnq4fwtpm757y6wbtirlc";
+export const grantType = "client_credentials";
