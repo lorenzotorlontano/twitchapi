9fb307814c8a9b1e144dc30c57f42d24f80fbb39
Palexis989 <piero.tupone@aesys.tech>
2020-09-23 10:06:17 +0200

    Merge with development

---
+++ src/Pages/Homepage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomeCarousel from '../../Components/HomeCarousel'
+
+export default function Homepage() {
+    return (
+        <div>
+            <HomeCarousel />
+        </div>
+    )
+}
