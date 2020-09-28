ac6dabd096e80d11d0eb151576dcff7655de38c6
Palexis989 <piero.tupone@aesys.tech>
2020-09-28 18:03:32 +0200

    Guarda che home Lorenzo...

--- src/Components/HomeSuggestedStreams/index.js
+++ src/Components/HomeSuggestedStreams/index.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { getSuggestedHomeStreams } from '../../Service/Api/Api'
 import ReactPlayer from "react-player"
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import { useStyles } from './styles';
 import Grid from '@material-ui/core/Grid';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 export default function HomeSuggestedStreams() {
+
+    const classes = useStyles();
 
     const [suggestedHomeStreams, setSuggestedStreams] = useState([])
 
@@ -17,33 +32,61 @@ export default function HomeSuggestedStreams() {
         getHomeSuggested()
     }, [])
 
-    const thumbnailFormatter = (url) => {
-        let formattedImg = url?.replace("{width}", "224");
-        let formattedImgFinal = formattedImg?.replace("{height}", "224");
-        return formattedImgFinal;
-    }
-
     console.log("Suggested Home Streams", suggestedHomeStreams)
 
     return (
+
         <div>
-            {suggestedHomeStreams && suggestedHomeStreams.map((img) => {
-                return (
 
-                    // <img src={`${thumbnailFormatter(img.thumbnail_url)}`}
-                    //     alt={"Not found"} key={`${img.id}`}
-                    // />
 
-                    <div>
-                        <ReactPlayer
-                            url={`https://www.twitch.tv/${img.user_name.replace(/\s+/g, '')}`}
-                            controls
-                        />
-                    </div>
 
-                )
-            })}
+            <div className={classes.root}>
+                <Grid container spacing={1} className={classes.videoWrapper}>
+                    {suggestedHomeStreams && suggestedHomeStreams.map((img) => {
+                        return (
+                            <Grid item xs={12} md={6} lg={4} >
+                                <Card className={classes.videoWrapper}>
 
-        </div>
+                                    <CardMedia>
+                                        {/* <ReactPlayer
+                                            className={classes.gridItem}
+                                            width={"380px"}
+                                            height={"270px"}
+                                            url={`https://www.twitch.tv/${img.user_name.replace(/\s+/g, '')}`}
+                                            controls={true}
+                                        // style={{ display: "flex", justifyContent: "space-evenly" }} */}
+                                        {/* /> */}
+                                    </CardMedia>
+
+                                    <CardContent>
+                                        <Grid container>
+
+                                            <Grid item xs={12}>
+                                                <ReactPlayer
+                                                    className={classes.gridItem}
+                                                    width={"380px"}
+                                                    height={"270px"}
+                                                    url={`https://www.twitch.tv/${img.user_name.replace(/\s+/g, '')}`}
+                                                    controls={true}
+                                                // style={{ display: "flex", justifyContent: "space-evenly" }}
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <h1>Some text</h1>
+                                            </Grid>
+                                        </Grid>
+                                    </CardContent>
+
+
+                                </Card>
+
+
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+            </div >
+
+        </div >
     )
 }
