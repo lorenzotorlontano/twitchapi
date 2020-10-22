da6d05b7e36d6f4c9f861c792cff87b50e81c399
tuonome <lorenzo.torlontano@gmail.com>
2020-10-22 16:42:35 +0200

    nce sta problema fratellì

--- src/Components/FollowingVideosTab/followingVideosTab.js
+++ src/Components/FollowingVideosTab/followingVideosTab.js
@@ -1,56 +1,59 @@
-import React, { useEffect, useState } from "react";
-import { getCurrentUserFollows, getVideosById } from "../../Service/Api/Api";
-import Grid from "@material-ui/core/Grid";
+import React, { useState, useEffect } from "react";
+import {
+  getCurrentUserFollows,
+  getUsers,
+  getVideosById,
+} from "../../Service/Api/Api";
 import ReactPlayer from "react-player";
+import Grid from "@material-ui/core/Grid";
 
-function FollowingVideosTab() {
-  const [myUsers, setMyUsers] = useState();
-  const [videos, setVideos] = useState();
+export default function FollowingVideosTab() {
+  const [myFollows, setMyFollows] = useState([]);
+  const [myFollowedChannels, setMyFollowedChannels] = useState([]);
 
   useEffect(() => {
-    const resp = getCurrentUserFollows().then((re) => {
-      setMyUsers(re.data.data);
-    });
+    const getMyFollows = async () => {
+      const res = await getCurrentUserFollows();
+      setMyFollows(res.data.data);
+    };
+    getMyFollows();
   }, []);
 
-  let arr = [];
-
-  myUsers &&
-    myUsers.map((val, index) => {
-      arr.push(val.to_id);
-    });
-
-  console.log("myuser", myUsers && myUsers);
-
-  console.log("arr", arr);
-
   useEffect(() => {
-    arr.map((va, i) => {
-      const res = getVideosById(va).then((re) => {
-        setVideos(re.data.data);
+    const promises = [];
+    if (!!myFollows.length) {
+      myFollows.forEach(
+        (val) => val.to_id && promises.push(getVideosById(val.to_id))
+      );
+      Promise.all(promises).then((responses) => {
+        const followedChannels = responses.map((res) => res.data.data[0]);
+        setMyFollowedChannels(followedChannels);
       });
-    });
-  }, []);
-
-  console.log("videos", videos && videos);
+    }
+  }, [myFollows]);
 
   return (
-    <Grid container style={{ color: "white", display: "flex" }}>
-      {videos &&
-        videos.map((val, index) => {
-          return (
-            <Grid item md={3}>
-              <ReactPlayer
-                width={"367px"}
-                height={"248px"}
-                url={`https://www.twitch.tv/${val.url.replace(/\s+/g, "")}`}
-              />
-              <span>{val.user_name}</span>
-            </Grid>
-          );
+    <Grid container>
+      {myFollowedChannels &&
+        myFollowedChannels.map((val, index) => {
+          console.log("val", val);
+          if (val !== undefined && index < 6) {
+            return (
+              <Grid style={{ color: "white" }} item md={4}>
+                <ReactPlayer
+                  width={"367px"}
+                  height={"248px"}
+                  url={`https://www.twitch.tv/${
+                    val && val.url.replace(/\s+/g, "")
+                  }`}
+                />
+                <span>{val.user_name}</span>
+              </Grid>
+            );
+          } else {
+            return null;
+          }
         })}
     </Grid>
   );
 }
-
-export default FollowingVideosTab;
