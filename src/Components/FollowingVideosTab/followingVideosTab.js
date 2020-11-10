e3cb2c40fb79bde47b90e9c592cfc66d147dddde
Palexis989 <piero.tupone@aesys.tech>
2020-11-10 16:23:51 +0100

    pierodev

--- src/Components/FollowingVideosTab/followingVideosTab.js
+++ src/Components/FollowingVideosTab/followingVideosTab.js
@@ -1,9 +1,4 @@
 import React, { useState, useEffect } from "react";
-<<<<<<< HEAD
-import { getCurrentUserFollows, getVideosById } from "../../Service/Api/Api";
-import ReactPlayer from "react-player";
-=======
->>>>>>> 3472c6715e531e5c5d95dbed8c72871e3c5df8c0
 import Grid from "@material-ui/core/Grid";
 import useGetMyFollows from "../../Hooks/useGetMyFollows";
 import VideosInfoDetails from "../FollowingVideosTab/VideosInfoDetails/index";
